bdc16b736bf21ebe9cc5c0fa76f580d8ea49f847
david-aihe <doa9200@gmail.com>
2022-07-16 12:04:44 -0400

    feature-Navbar-completed navbar functinality and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,51 @@
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
+import { images } from '../constants';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggle, setToggle] = useState(false);
 
-export default Navbar
+  return (
+    <nav className="w-full flex justify-between items-center py-4 px-8 bg-primary fixed z-[2]">
+      <div className="flex justify-start items-center">
+        <img src={images.logo} alt="logo" className='w-52 xl:w-[300px]' />
+      </div>
+
+      <ul className="flex-1 hidden ml:flex justify-center items-center list-none">
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text my-0 mx-4 cursor-pointer flex-col" key={`link-${item}`}>
+            <div className='w-[5px] h-[5px] bg-transparent rounded-[50%] mb-[5px] hover:bg-secondary' />
+            <a href={`#${item}`} className='text-gray no-underline flex-col uppercase font-medium transition-all duration-300 ease-in-out hover:text-secondary'>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="w-9 h-9 rounded-[50%] relative flex ml:hidden justify-center items-center bg-secondary">
+        <HiMenuAlt4 onClick={() => setToggle(true)} className='w-[70%] h-[70%] text-white cursor-pointer' />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+            className='fixed top-0 bottom-0 right-0 z-[5] p-4 w-4/5 h-screen flex justify-end items-end flex-col bg-white shadow-[0px 0px 20px rgba(168, 168, 168, 0.15)]'
+          >
+            <HiX onClick={() => setToggle(false)} className='w-9 h-9 text-secondary my-2 mx-4 cursor-pointer' />
+            <ul className='list-none m-0 p-0 h-full w-full flex ml:hidden justify-start items-start flex-col '>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item} className='m-4' >
+                  <a href={`#${item}`} onClick={() => setToggle(false)} className='text-gray no-underline text-base uppercase font-medium transition-all duration-300 ease-in-out hover:text-secondary' >
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
