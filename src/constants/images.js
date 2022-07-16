6310ba2b7d95a086190f812ebfe0ea128b2ee7f4
david-aihe <doa9200@gmail.com>
2022-07-16 12:51:21 -0400

    feature-Header-structure in place

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,13 @@
 import logo from '../assets/logo.png';
+import ball1 from '../assets/ball1.png';
+import ball2 from '../assets/ball2.png';
+import player from '../assets/player.png';
+import profile from '../assets/profile.png';
 
 export default {
     logo,
+    ball1,
+    ball2,
+    player,
+    profile,
 };
