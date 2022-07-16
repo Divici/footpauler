36fa1875fe6a6f24b90ca12e8c8436fae874f229
david-aihe <doa9200@gmail.com>
2022-07-16 11:07:01 -0400

    Some setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '450px',
+      md: '650px',
+      ml: '850px',
+      lg: '1150px',
+      xl: '2750px',
+    },
+    extend: {
+      fontFamily: {
+        base: ["DM Sans", "sans-serif"],
+      },
+      colors: {
+        'primary': '#eff8ed',
+        'secondary': '#31ac48',
+        'black': '#030303',
+        'lightGray': '#e4e4e4',
+        'gray': '#6b7688',
+        'brown': '#384a36',
+        'white': '#ffffff',
+      },
+    },
   },
   plugins: [],
 }
