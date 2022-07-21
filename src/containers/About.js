f21d9195d448feb916883d41cdd68eb4857f0850
david-aihe <doa9200@gmail.com>
2022-07-21 14:34:30 -0400

    feature-About-Finished Section.

--- src/containers/About.js
+++ src/containers/About.js
@@ -12,8 +12,9 @@ const About = () => {
       >
         <div className='w-full lg:w-3/5'>
           <h2 className="bold-text text-center mt-4 uppercase">About Me</h2>
-          <p className="p-text px-4 text-center text-base ml:text-lg mt-3">Semi-Pro Footballer & Licensed Coach Drills, skills, training, highlights, and everything football.</p>
-          <p className='p-text p-4 text-center text-black'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptat.</p>
+          <p className="p-text px-4 text-center text-base ml:text-lg mt-3">Semi-Pro Footballer & Licensed Coach</p>
+          <p className='p-text p-4 text-center text-black'>Play the game on a Semi -professional level and involved as a Head Coach both in Womens and Mens Games.</p>
+          <p className='p-text p-4 text-center text-black'>At the moment I am involved as a Lead Coach and Supervisor at MSI (Montgomery Soccer Inc) working with young players and trying to improve coaches in the organization</p>
         </div>
         <div className=''>
           <img src={images.aboutme} alt='me' className='h-96 ml:h-[450px] lg:h-1/2 object-contain' />
